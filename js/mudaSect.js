b96196a1739e0d784c214329a3ae00180348be3e
Bianca Amorim <biaancaamorim@gmail.com>
2018-10-06 11:32:41 -0400

    O primeiro commit

---
+++ js/mudaSect.js
@@ -0,0 +1,40 @@
+$(function(){
+    
+    var pagePositon = 0,
+        sectionsSeclector = 'section',
+        $scrollItems = $(sectionsSeclector),
+        offsetTolorence = 30,
+        pageMaxPosition = $scrollItems.length - 1;
+    
+    $scrollItems.each(function(index,ele) { $(ele).attr("debog",index).data("pos",index); });
+
+    $(window).bind('scroll',upPos);
+    
+    $('.arrow a').click(function(e){
+        if ($(this).hasClass('next') && pagePositon+1 <= pageMaxPosition) {
+            pagePositon++;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top
+            }, 300);
+        }
+        if ($(this).hasClass('previous') && pagePositon-1 >= 0) {
+            pagePositon--;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top
+              }, 300);
+            return false;
+        }
+    });
+    
+    function upPos(){
+       var fromTop = $(this).scrollTop();
+       var $cur = null;
+        $scrollItems.each(function(index,ele){
+            if ($(ele).offset().top < fromTop + offsetTolorence) $cur = $(ele);
+        });
+       if ($cur != null && pagePositon != $cur.data('pos')) {
+           pagePositon = $cur.data('pos');
+       }                   
+    }
+    
+});
